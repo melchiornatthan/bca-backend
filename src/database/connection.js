28bec8f88bcc9bc6da22088719f90117bde5546d
Melchior Natthan <85684770+melchiornatthan@users.noreply.github.com>
2024-01-24 17:12:16 +0700

    Update database connection

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 
 // Configure Sequelize for connecting to the 'bca' database on localhost
-const sequelize = new Sequelize('bca', 'melchiornatthan', '', {
-  host: 'localhost', // Change this to your database host
+const sequelize = new Sequelize('postgres', 'postgres', 'bca123123123', {
+  host: 'db', // Change this to your database host
+  port: 5432, // Change this to your database port
   dialect: 'postgres', // Use 'mysql' for MySQL, 'sqlite' for SQLite, etc.
+  logging: false, // Should be set to false for production
 });
 
 // Export the Sequelize instance for use in other parts of your application
